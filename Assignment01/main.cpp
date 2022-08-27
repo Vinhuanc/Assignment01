f13d37735a85b350381c5353a20de2141db9ded3
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-08-27 08:55:42 -0500

    Initial Commit

---
+++ Assignment01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment01
+//
+//  Created by Chen, Huan on 8/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
