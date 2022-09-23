71b38bb5c4ec98645d6eecd21dd63287b2d7a98f
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-09-22 20:27:26 -0500

    Update main.cpp

--- Assignment01-Program03/main.cpp
+++ Assignment01-Program03/main.cpp
@@ -1,4 +1,11 @@
-
+// Program #3 (Id: 165546)
+// Write a program that will calculate the total for a retail sale. The program should ask the user for the retail price of the item being purchased and the sales tax rate. Once these items have been entered, the program should calculate and display the sales tax for the purchase and the total of the sale.
+// The following messages should be used with interfacing with the user: “Please enter the retail price:”
+// “Please enter the tax rate:”
+// “The sales tax is: $”
+// “The total price is: $”
+// Hint: Copy and modify the previous programs! A good variable definition statement would be: double price, taxRate, salesTax, total;
+// Hint: the tax rate will be entered as if it was [value]%, this means you need to modify the value in order to use it in a numeric calculation.
 #include <iostream>
 using namespace std;
 
