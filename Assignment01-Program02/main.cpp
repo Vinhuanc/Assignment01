c01dc2e9eec698ce90bbff16b4dee8897aa9c661
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-09-22 20:27:06 -0500

    Update main.cpp

--- Assignment01-Program02/main.cpp
+++ Assignment01-Program02/main.cpp
@@ -5,7 +5,12 @@
 //
 //  Created by Chen, Huan on 8/27/22.
 //
-
+// Program #2 (Id: 165435)
+// Write a program that will ask the user to enter their maximum credit and credit already used. The program should then calculate their total remaining credit. Use the source code shown below, adding code as necessary without modifying the existing code.
+// The following messages should be used with interfacing with the user:
+// “What is your maximum credit?”
+// “How much credit have you already used?” “Your remaining credit is $”
+ 
 //#include "ProgramTwo.hpp"
 #include <iostream>
 using namespace std;
